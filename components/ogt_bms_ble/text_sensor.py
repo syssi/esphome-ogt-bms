c14f046c8b885cea1eba35eae834dfaeb89981bd
Sebastian Muszynski <basti@linkt.de>
2025-03-21 09:23:09 +0100

    Expose `serial number` and `manufacture date` as text sensor (#22)

--- components/ogt_bms_ble/text_sensor.py
+++ components/ogt_bms_ble/text_sensor.py
@@ -12,11 +12,15 @@ CODEOWNERS = ["@syssi"]
 CONF_ERRORS = "errors"
 CONF_TIME_TO_EMPTY_FORMATTED = "time_to_empty_formatted"
 CONF_TIME_TO_FULL_FORMATTED = "time_to_full_formatted"
+CONF_SERIAL_NUMBER = "serial_number"
+CONF_MANUFACTURE_DATE = "manufacture_date"
 
 TEXT_SENSORS = [
     CONF_ERRORS,
     CONF_TIME_TO_EMPTY_FORMATTED,
     CONF_TIME_TO_FULL_FORMATTED,
+    CONF_SERIAL_NUMBER,
+    CONF_MANUFACTURE_DATE,
 ]
 
 CONFIG_SCHEMA = cv.Schema(
@@ -40,6 +44,18 @@ CONFIG_SCHEMA = cv.Schema(
             {
                 cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
                 cv.Optional(CONF_ICON, default="mdi:battery-charging-100"): cv.icon,
+            }
+        ),
+        cv.Optional(CONF_SERIAL_NUMBER): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
+                cv.Optional(CONF_ICON, default="mdi:numeric"): cv.icon,
+            }
+        ),
+        cv.Optional(CONF_MANUFACTURE_DATE): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
+                cv.Optional(CONF_ICON, default="mdi:factory"): cv.icon,
             }
         ),
     }
