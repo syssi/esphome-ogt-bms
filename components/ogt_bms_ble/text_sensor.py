c5c6c5bbf70d5ef058d622e15118f13bea892991
Sebastian Muszynski <basti@linkt.de>
2026-03-25 10:49:00 +0100

    Apply pre-commit fixes

--- components/ogt_bms_ble/text_sensor.py
+++ components/ogt_bms_ble/text_sensor.py
@@ -1,7 +1,6 @@
 import esphome.codegen as cg
 from esphome.components import text_sensor
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
 
 from . import OGT_BMS_BLE_COMPONENT_SCHEMA
 
