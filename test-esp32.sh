742dfc71edc619afb5b3613186e1e852602eff44
Sebastian Muszynski <basti@linkt.de>
2026-05-19 15:22:49 +0200

    Move BLE name to secrets.yaml
    Reference the BLE device name via `!secret bms0_ble_name` directly at
    the component level instead of routing through a YAML substitution.
    Remove the now-unused substitution entries and shorten test-esp32.sh.

--- test-esp32.sh
+++ test-esp32.sh
@@ -6,4 +6,4 @@ else
   C="components"
 fi
 
-esphome -s ble_name SmartBat-A03747 -s external_components_source $C ${1:-run} ${2:-esp32-ble-example-faker.yaml}
+esphome -s external_components_source $C ${1:-run} ${2:-esp32-ble-example-faker.yaml}
